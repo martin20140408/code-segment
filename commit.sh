7d0f70cc8343ae77d3536bce0ae0164e85cf690f
martin <martin@achang.com>
2016-09-11 22:37:29 +0800

    update

--- commit.sh
+++ commit.sh
@@ -1,5 +1,7 @@
+time=$(date +%Y_%m_%d_%H_%i_%s)
 git pull origin master
 git status
 git add -A
-git commit -m "update"
+git commit -m $(time)"update"
+git tag -a "v"$(time) -m $(time)
 git push origin master
