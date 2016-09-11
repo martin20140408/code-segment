d809475679447a51d2596e1d2b8993631af43c97
martin <martin@achang.com>
2016-09-11 22:41:38 +0800

    update

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 git status
 git add -A
 git commit -m $(time)"update"
-git tag -a $(date +%Y_%m_%d_%H) -m "package code on "$(date +%Y_%m_%d)
+git tag -a $(date +%Y_%m_%d_%H_%s) -m "package code on "$(date +%Y_%m_%d)
 git pull origin master
 git push origin master
